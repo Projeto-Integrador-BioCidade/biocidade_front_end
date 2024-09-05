20f10927c08cbb171226577df5c762c3655c026b
pierresouza <pierre.s3@hotmail.com>
2024-09-05 15:33:01 -0300

    fix: change name to correct route

--- src/components/category/card/CardCategory.tsx
+++ src/components/category/card/CardCategory.tsx
@@ -13,7 +13,7 @@ function CardCategory({ categoria }: CardCategoryProps) {
         <p>{categoria.nome}</p>
       </div>
       <div className="flex">
-        <Link to={`/editarcategories/${categoria.id}`} className="w-full text-black font-bold transition ease-in-out bg-gray-400 hover:bg-bio-City-grey flex items-center justify-center py-2">
+        <Link to={`/editcategories/${categoria.id}`} className="w-full text-black font-bold transition ease-in-out bg-gray-400 hover:bg-bio-City-grey flex items-center justify-center py-2">
           <button>Editar</button>
         </Link>
         <Link to={`/deletcategories/${categoria.id}`} className="text-black font-bold transition ease-in-out hover:bg-lime-400 bg-bio-City-input-color w-full flex items-center justify-center">
