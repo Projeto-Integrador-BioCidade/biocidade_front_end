83961d3989eaf27e0f43df1eb453b5162e2261eb
pierresouza <pierre.s3@hotmail.com>
2024-09-05 14:49:21 -0300

    feat: create styles from cardCategory

--- src/components/category/card/CardCategory.tsx
+++ src/components/category/card/CardCategory.tsx
@@ -1,21 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import Categoria from "../../../models/categoria";
 
 interface CardCategoryProps {
-  categoria: Categoria
+  categoria: Categoria;
 }
 
 function CardCategory({ categoria }: CardCategoryProps) {
   return (
-    <div>
-      <header>Categoria</header>
-      <p>{categoria.nome}</p>
-      <div>
-        <Link to={`/editcategories/${categoria.id}`}>
+    <div className="min-w-[300px] max-w-[500px]  border border-black rounded-lg flex flex-col">
+      <div className="py-2 px-4 font-medium flex flex-col gap-4">
+        <header>Categoria</header>
+        <p>{categoria.nome}</p>
+      </div>
+      <div className="flex">
+        <Link to={`/editarPostagem/${categoria.id}`} className="w-full text-black font-bold transition ease-in-out bg-gray-400 hover:bg-bio-City-grey flex items-center justify-center py-2">
           <button>Editar</button>
         </Link>
-
-        <Link to={`/deletcategories/${categoria.id}`}>
+        <Link to={`/deletarPostagem/${categoria.id}`} className="text-black font-bold transition ease-in-out hover:bg-lime-400 bg-bio-City-input-color w-full flex items-center justify-center">
           <button>Deletar</button>
         </Link>
       </div>
