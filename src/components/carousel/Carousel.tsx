dd0a41ae6d27640c5539de1b68ff464147e12b20
lucas_souza_ribeiro <lucas.srs.2022@gmail.com>
2024-09-06 13:29:59 -0300

    feat: HomePage

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,8 +1,46 @@
 import { ArrowLeft, ArrowRight, Dot } from "@phosphor-icons/react";
-import { SetStateAction, useState } from "react";
+import React, { SetStateAction, useState } from "react";
 
 
 function Carrosel() {
+  const [startPosX, setStartPosX] = useState(0)
+  const [endPosX, setEndPosX] = useState(0)
+
+  const handleSwipe = () => {
+    const distance = startPosX - endPosX;
+    const swipeThreshold = 50;
+
+    if (distance > swipeThreshold) {
+      nextSlide()
+    } else if (distance < -swipeThreshold) {
+      prevSlide()
+    }
+  }
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setStartPosX(e.clientX);
+  }
+
+  const handleMouseUp = (e: React.MouseEvent) => {
+    setEndPosX(e.clientX);
+    handleSwipe();
+  }
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setStartPosX(e.touches[0].clientX)
+  }
+
+  const handleTouchEnd = (e: React.TouchEvent) => {
+    setEndPosX(e.changedTouches[0].clientX)
+    handleSwipe();
+  }
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    e.preventDefault();
+  }
+
+
+
   const slides = [
     {
       imagem: 'https://ik.imagekit.io/o4h22lltho/BioCidade%20/produtos_ecologicos.png?updatedAt=1725542912860',
@@ -26,28 +64,14 @@ function Carrosel() {
       texto: 'Tijolos feitos de uma mistura de solo arenoso, cimento e água, que são prensados para adquirir solidez e formatos desejados.',
       bgColor: 'from-orange-700 from-50% to-red-300',
       btnColor: 'bg-orange-700 hover:bg-orange-800',
-      btnText: 'visitar',
-    },
-
-    {
-      imagem: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-      titulo: '',
-      texto: '',
-      bgColor: '',
-      btnColor: '',
-      btnText: '',
-    },
-    {
-      imagem: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-      titulo: '',
-      texto: '',
-      bgColor: '',
-      btnColor: '',
-      btnText: '',
-    },
+      btnText: 'comprar',
+    }
   ];
 
+
+
   const [currentIndex, setCurrentIndex] = useState(0);
+
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
@@ -66,35 +90,43 @@ function Carrosel() {
   };
 
   return (
-    <div className={`flex h-[30vh] relative group bg-gradient-to-r rounded-b-2xl ${slides[currentIndex].bgColor}`}>
+    <div className={`mb-5 flex flex-col justify-center relative group bg-gradient-to-r rounded-b-2xl ${slides[currentIndex].bgColor} h-80`}>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].imagem})` }}
-        className='bg-left w-full h-full  bg-[length:80%_200%] bg-no-repeat duration-500 opacity-70 rounded-bl-2xl'
+        className='flex justify-center bg-left w-full h-full  bg-[length:100%_100%] bg-no-repeat duration-500 rounded-b-2xl
+                  md:bg-[length:50%_100%] md:justify-end xl:bg-[length:40%_100%]'
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+        onTouchMove={handleTouchMove}
       >
-      </div>
-        <div className="h-full flex flex-col justify-between items-center w-1/2 py-4 font-roboto *:duration-300">
-          <h2 className="text-5xl font-bold">{slides[currentIndex].titulo}</h2>
-          <p className="text-center text-3xl">{slides[currentIndex].texto}</p>
-          <button className={`h-14 w-48 rounded-2xl uppercase duration-100 text-2xl font-semibold hover:font-extrabold ${slides[currentIndex].btnColor}`}>{slides[currentIndex].btnText}</button>
+        <div className="text-center w-full py-5 px-6 text-black h-full flex flex-col justify-between items-center font-roboto *:duration-300 
+                        md:w-1/2 xl:w-[60%] xl:px-52"
+                        style={{textShadow: '1px 1px 4px rgba(255, 255, 255, 0.8)'}}>
+          <h2 className="text-2xl md:text-4xl font-bold">{slides[currentIndex].titulo}</h2>
+          <p className="text-lg md:text-2xl font-semibold my-5">{slides[currentIndex].texto}</p>
+          <button className={`flex items-center justify-center rounded-2xl uppercase duration-100 text-2xl font-semibold p-2 hover:font-extrabold ${slides[currentIndex].btnColor}`}>{slides[currentIndex].btnText}</button>
         </div>
+      </div>
 
 
       {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <ArrowLeft color="#fcfcfc" weight="bold" onClick={prevSlide} size={30} />
+        <ArrowLeft size={22} color="#fcfcfc" weight="bold" onClick={prevSlide} />
       </div>
       {/* Right Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <ArrowRight size={30} color="#fcfcfc" weight="bold" onClick={nextSlide} />
+        <ArrowRight size={22} color="#fcfcfc" weight="bold" onClick={nextSlide} />
       </div>
-      <div className='flex top-4 justify-center'>
+      <div className='flex justify-center'>
         {slides.map((slide, slideIndex) => (
           <div
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer h-2'
+            className=' cursor-pointer h-0'
           >
-            <Dot size={32} color="#fcfcfc" weight="bold" />
+            <Dot size={32} color="#000000" weight="bold" />
           </div>
         ))}
       </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,27 @@
 import Carousel from "../../components/carousel/Carousel";
+import ListCategory from "../../components/category/list/ListCategory";
+import ListProduct from "../../components/product/list/ListProduct";
 
 function Home() {
   return (
     <>
-      <Carousel/>
+      <div className="bg-gray-200 font-roboto">
+        <Carousel />
+        <div className="mx-10">
+          <div>
+            <h2 className="text-4xl font-bold mb-4">Novidades</h2>
+            <ListProduct />
+          </div>
+          <hr className="border border-b-0 border-black opacity-50 mt-6" />
+          <div>
+            <h2 className="text-4xl font-bold mb-4">Categorias</h2>
+            <ListCategory />
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+
     </>
   );
 }
