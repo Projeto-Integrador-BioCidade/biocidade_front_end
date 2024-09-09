9016e47450ec3a0a02af8ee6c117b21236a6bd96
Joshua Simionato <joshua.simionato@gmail.com>
2024-09-09 15:57:39 -0300

    Carrinho

--- src/App.tsx
+++ src/App.tsx
@@ -14,34 +14,40 @@ import DeleteProduct from "./components/product/delete/DeleteProduct";
 import FormProduct from "./components/product/form/FormProduct";
 import ListProduct from "./components/product/list/ListProduct";
 import Perfil from "./pages/perfil/Perfil";
+import { CartProvider } from "./contexts/CartContext";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className="bg-bio-City-cream">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/categories" element={<ListCategory />} />
-              <Route path="/editcategories/:id" element={<FormCategory />} />
-              <Route path="/deletcategories/:id" element={<DeleteCategory />} />
-              <Route path="/products" element={<ListProduct />} />
-              <Route path="/products/:id" element={<ListProduct />} />
-              <Route path="/cadproducts" element={<FormProduct />} />
-              <Route path="/editproducts/:id" element={<FormProduct />} />
-              <Route path="/deletproducts/:id" element={<DeleteProduct />} />
-              <Route path="/perfil" element={<Perfil />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-      </AuthProvider>
+      <CartProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <Navbar />
+            <div className="bg-bio-City-cream">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/categories" element={<ListCategory />} />
+                <Route path="/editcategories/:id" element={<FormCategory />} />
+                <Route path="/deletcategories/:id" element={<DeleteCategory />} />
+                <Route path="/products" element={<ListProduct />} />
+                <Route path="/products/:id" element={<ListProduct />} />
+                <Route path="/cadproducts" element={<FormProduct />} />
+                <Route path="/editproducts/:id" element={<FormProduct />} />
+                <Route path="/deletproducts/:id" element={<DeleteProduct />} />
+                <Route path="/perfil" element={<Perfil />} />
+                <Route path="/cart" element={<Cart />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
+      </CartProvider>
+
     </>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,7 @@
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext";
-import BioLogo from "../../assets/navbar_logo.png";
-
-import {
-  List,
-  MagnifyingGlass,
-  SignIn,
-  SignOut,
-  User,
-} from "@phosphor-icons/react";
-import { ToastAlerta } from "../../utils/ToastAlerta";
+import { List, MagnifyingGlass, ShoppingCart, SignIn, SignOut, User } from "@phosphor-icons/react";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,123 +11,94 @@ function Navbar() {
 
   function logout() {
     handleLogout();
-    ToastAlerta("O usuário foi desconectado!", "info");
+    alert("O usuário foi desconectado!");
     navigate("/");
   }
 
-  let navbarComponent;
 
-  if (usuario.token !== "") {
-    navbarComponent = (
-      <>
-        <nav className="w-full bg-bio-City-cream p-6 text-lg">
-          <div
-            className={`mx-auto items-center ${isOpen ? "flex-col" : "flex md:justify-between"} justify-start`}
-          >
-            <div className="flex h-20 md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                className="text-black transition-all duration-500 ease-in-out hover:text-white focus:outline-none"
-              >
-                {isOpen ? (
-                  <List size={28} color="gray" />
-                ) : (
-                  <List size={28} color="black" />
-                )}
-              </button>
-            </div>
-            <Link to={"/"}>
-              <img
-                className={`mx-4 h-20 rounded-full ${isOpen ? "hidden" : "block"}`}
-                src={BioLogo}
-                alt="Logo BioCidade"
-              />
-            </Link>
-
-            <div>
-              <form action="" className="flex">
-                <input
-                  type="text"
-                  id="pesquisa"
-                  name="pesquisa"
-                  className="m-2 h-7 min-w-[250px] max-w-96 rounded-full border-2 border-solid border-bio-City-footer-navbar-color px-2 placeholder:px-1 placeholder:italic placeholder:text-slate-400 enabled:hover:border-gray-400 sm:text-sm"
-                  placeholder="Pesquisar produtos"
-                />
-                <button>
-                  <MagnifyingGlass size={20} color="black" />
-                </button>
-              </form>
-            </div>
-
-            <div
-              className={`w-full md:flex md:w-auto ${isOpen ? "block" : "hidden"} `}
-            >
-              <ul className="flex flex-col text-nowrap font-sans text-xs font-bold md:flex-row md:items-center md:gap-3 md:text-sm lg:gap-5 lg:text-lg">
-                <Link to={"/"}>
-                  <li className="text-black transition-all duration-500 ease-in-out hover:text-white">
-                    Home
-                  </li>
-                </Link>
-                <Link to={"/products"}>
-                  <li className="text-black transition-all duration-500 ease-in-out hover:text-white">
-                    Produtos
-                  </li>
-                </Link>
-                {usuario.tipo === "VENDEDOR" ? (
-                  <Link to="/cadproducts">
-                    <li className="text-black transition-all duration-500 ease-in-out hover:text-white">
-                      Cadastrar Produtos
-                    </li>
-                  </Link>
-                ) : (
-                  ""
-                )}
-
-                <Link to={"/categories"} title="Categorias">
-                  <li className="text-black transition-all duration-500 ease-in-out hover:text-white">
-                    Categoria
-                  </li>
-                </Link>
-                {usuario.token === "" ? (
-                  <Link to={"/register"} title="Cadastre-se">
-                    <li className="text-black transition-all duration-500 ease-in-out hover:text-white">
-                      Cadastre-se
-                    </li>
-                  </Link>
-                ) : (
-                  ""
-                )}
-                {usuario.token === "" ? (
-                  <Link to={"/login"}>
-                    <SignIn size={24} color="black" alt="Login" />
-                  </Link>
-                ) : (
-                  ""
-                )}
-
-                {usuario.token !== "" ? (
-                  <Link to={"/perfil"}>
-                    <User size={24} color="black" alt="Perfil de usuário" />
-                  </Link>
-                ) : (
-                  ""
-                )}
-
-                {usuario.token !== "" ? (
-                  <Link to="" onClick={logout} title="Sair">
-                    <SignOut size={24} color="black" alt="Sair" />
-                  </Link>
-                ) : (
-                  ""
-                )}
-              </ul>
-            </div>
+  return (
+    <>
+      <nav className="w-full p-6 bg-bio-City-main-green text-lg">
+        <div className={`mx-auto items-center ${isOpen ? "flex-col " : "flex justify-between "} `}>
+          <div className="flex md:hidden h-20">
+            <button onClick={() => setIsOpen(!isOpen)} className="text-black hover:text-white focus:outline-none ">
+              {isOpen ? <List size={28} color="gray" /> : <List size={28} color="black" />}
+            </button>
           </div>
-        </nav>
-      </>
-    );
-  }
-  return <>{navbarComponent}</>;
+          <Link to={"/"}>
+            <img className={`h-20 rounded-full mx-4 ${isOpen ? "hidden" : "block"}`} src="src/assets/logoBioCidade.png" alt="Logo BioCidade" />
+          </Link>
+
+          <div>
+            <form action="" className="flex">
+              <input type="text" id="pesquisa" name="pesquisa" className="px-2 w-96 h-7 placeholder:italic placeholder:text-slate-400 placeholder:px-1 border-solid border-2 border-green-400 rounded-full m-2 enabled:hover:border-gray-400 sm:text-sm" placeholder="Pesquisar produtos" />
+              <button><MagnifyingGlass size={20} /></button>
+            </form>
+          </div>
+
+          <div className={`w-full md:w-auto md:flex  ${isOpen ? "block" : "hidden"} `}>
+            <ul className="flex flex-col md:flex-row md:items-center md:gap-3 font-sans text-xs text-nowrap md:text-sm lg:gap-5 lg:text-lg ">
+              <Link to={"/"}>
+                <li className=" text-black hover:text-white">Home</li>
+              </Link>
+              <Link to={"/products"}>
+                <li className=" text-black hover:text-white">Produtos</li>
+              </Link>
+              {usuario.tipo === 'VENDEDOR' ? (
+                <Link to="/cadproducts">
+                  <li className=" text-black hover:text-white">Cadastrar Produtos</li>
+                </Link>
+              ) : (
+                ""
+              )}
+
+              <Link to={"/categories"}>
+                <li className=" text-black hover:text-white">Categoria</li>
+              </Link>
+              {usuario.token === "" ? (
+                <Link to={"/register"}>
+                  <li className=" text-black hover:text-white">Cadastre-se</li>
+                </Link>
+              ) : (
+                ""
+              )}
+              {usuario.token === "" ? (
+                <Link to={"/login"}>
+                  <SignIn size={24} color="black" />
+                </Link>
+              ) : (
+                ""
+              )}
+
+              {usuario.token !== "" ? (
+                <Link to='/cart'><ShoppingCart size={24} /></Link>
+              ) : (
+                ""
+              )}
+
+              {usuario.token !== "" ? (
+                <Link to={"/perfil"}>
+                  <User size={24} color="black" />
+                </Link>
+              ) : (
+                ""
+              )}
+
+              {usuario.token !== "" ? (
+                <Link to="" onClick={logout} title="Sair">
+                  <SignOut size={24} color="black" />
+                </Link>
+              ) : (
+                ""
+              )}
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    </>
+  );
 }
 
 export default Navbar;
